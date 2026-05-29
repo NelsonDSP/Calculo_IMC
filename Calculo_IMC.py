071873a44845eb1e91c91a2d42682a682e252986
Nnelsan <nelsondsp20@gmail.com>
2026-05-29 12:47:50 +0100

    Update Calculo_IMC.py

--- Calculo_IMC.py
+++ Calculo_IMC.py
@@ -1,7 +1,9 @@
 # Sprint 2 - Classificação do IMC
 
-peso = float(input("Introduza o seu peso (kg): "))
-altura = float(input("Introduza a sua altura (cm): "))
+def calcular_imc(peso, altura):
+    altura_metros = altura / 100
+    imc = peso / (altura_metros * altura_metros)
+    return round(imc, 2)
  
 altura_metros = altura / 100
 imc = peso / (altura_metros * altura_metros)
