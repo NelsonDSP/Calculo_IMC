d8de434e2400db527b1cc4113b338872484d6269
Nnelsan <nelsondsp20@gmail.com>
2026-05-29 12:23:17 +0100

    Update Calculo_IMC.py

--- Calculo_IMC.py
+++ Calculo_IMC.py
@@ -5,3 +5,14 @@ altura = float(input("Introduza a sua altura (cm): "))
  
 altura_metros = altura / 100
 imc = peso / (altura_metros * altura_metros)
+ 
+imc = round(imc, 2)
+ 
+print(f"IMC: {imc}")
+ 
+if imc < 18.5:
+    print("Fora do peso normal")
+elif imc >= 18.5 and imc < 25:
+    print("Peso normal")
+else:
+    print("Fora do peso normal")
