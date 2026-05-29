4677726791fdd469874e51d926e483dbcde9e8f4
Nnelsan <nelsondsp20@gmail.com>
2026-05-29 12:48:47 +0100

    Update Calculo_IMC.py

--- Calculo_IMC.py
+++ Calculo_IMC.py
@@ -5,17 +5,13 @@ def calcular_imc(peso, altura):
     imc = peso / (altura_metros * altura_metros)
     return round(imc, 2)
  
-altura_metros = altura / 100
-imc = peso / (altura_metros * altura_metros)
- 
-imc = round(imc, 2)
- 
-print(f"IMC: {imc}")
- 
-if imc < 18.5:
-    print("Fora do peso normal")
-elif imc >= 18.5 and imc < 25:
-    print("Peso normal")
-else:
-    print("Fora do peso normal")
+def classificar_imc(imc):
+    if imc < 18.5:
+        return "Abaixo do peso"
+    elif imc < 25:
+        return "Peso normal"
+    elif imc < 30:
+        return "Excesso de peso"
+    else:
+        return "Obesidade"
     
