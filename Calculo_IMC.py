a7fa23b25eefb35562ff7add2a713da97a8d7374
Nnelsan <nelsondsp20@gmail.com>
2026-05-29 12:21:42 +0100

    Update Calculo_IMC.py

--- Calculo_IMC.py
+++ Calculo_IMC.py
@@ -1,3 +1,7 @@
+#A fórmula do IMC é: IMC = peso / (altura_em_metros * altura_em_metros)
+
 peso = float(input("Introduza o seu peso (kg): "))
 altura = float(input("Introduza a sua altura (cm): "))
  
+altura_metros = altura / 100
+imc = peso / (altura_metros * altura_metros)
